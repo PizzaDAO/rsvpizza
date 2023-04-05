edcc6e36b58475e89a5009801c7bd1b316ac223d
Daniel van de Ghinste <danielvandeghinste@gmail.com>
2023-04-05 16:38:21 +0200

    added chakra-ui and custom theme extension structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,19 @@ import { type AppType } from 'next/app';
 
 import { api } from '~/utils/api';
 
+import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+import theme from '../theme';
+
 import '~/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<ClerkProvider {...pageProps}>
-			<Component {...pageProps} />
-		</ClerkProvider>
+		<ChakraProvider theme={theme}>
+			<ClerkProvider {...pageProps}>
+				<Component {...pageProps} />
+			</ClerkProvider>
+		</ChakraProvider>
 	);
 };
 
