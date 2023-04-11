3d17eaba63a13818c4027a5717d6372b8d664de7
Don Luv <danielvandeghinste@gmail.com>
2023-04-11 12:59:01 +0200

    Add unstyled homepage (#8)
    * blank project setup complete
    
    * template maybe complete
    
    * Feature/event schema and crud (#4)
    
    * CreateEvent nearly working
    
    * realigned schema
    
    * form working

---
+++ src/components/Event/CreateEvent/Component.tsx
@@ -0,0 +1,52 @@
+import { Box, Flex, Heading, VStack } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import { EventForm } from './EventForm';
+import { EventFormData } from './EventSchema';
+import { useUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import { api } from '~/utils/api';
+import generateUniqueSlug from '~/utils/generateUniqueSlug';
+
+const CreateEvent: NextPage = () => {
+	const { user } = useUser();
+	console.log(user);
+	if (!user) {
+		return null;
+	}
+
+	const { mutate } = api.events.create.useMutation();
+
+	const handleFormSubmit = (data: EventFormData) => {
+		// Handle form submission here, such as calling an API or updating state
+		console.log('Form submitted with data:', data);
+		const newData = { ...data, slug: generateUniqueSlug() };
+		mutate(newData);
+	};
+
+	return (
+		<Flex
+			position={'relative'}
+			flexDirection={'row'}
+			justifyContent={'flex-start'}
+			bgColor={'blue.500'}
+			w={'500px'}
+		>
+			<Flex position={'absolute'} top={'0'} left={'0'}>
+				<Image
+					src={user.profileImageUrl}
+					alt={'Profile image'}
+					width={128}
+					height={128}
+				/>
+			</Flex>
+			<VStack spacing={6}>
+				<Heading>Create Event</Heading>
+				<Box width='100%' maxWidth='600px'>
+					<EventForm onSubmit={handleFormSubmit} />
+				</Box>
+			</VStack>
+		</Flex>
+	);
+};
+
+export default CreateEvent;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,53 @@
 import { extendTheme } from '@chakra-ui/react';
 
-export const theme = extendTheme({});
+export const theme = extendTheme({
+	styles: {
+		global: {
+			// Add your global styles here
+			html: {
+				padding: 0,
+				margin: 0,
+			},
+			body: {
+				padding: 0,
+				margin: 0,
+				fontFamily:
+					'-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif',
+				color: 'white',
+			},
+			a: {
+				color: 'inherit',
+				textDecoration: 'none',
+			},
+			'*': {
+				boxSizing: 'border-box',
+			},
+			// Add your custom styles here
+			'.rdt': {
+				position: 'relative',
+			},
+			'.rdtPicker': {
+				background: 'white',
+				border: '1px solid #ccc',
+				padding: '10px',
+				borderRadius: '3px',
+				zIndex: '1000',
+			},
+			'.autocomplete-dropdown-container': {
+				position: 'relative',
+				width: '100%',
+			},
+			'.autocomplete-dropdown': {
+				position: 'absolute',
+				zIndex: '1000',
+				width: '100%',
+				background: 'white',
+				border: '1px solid #ccc',
+				borderRadius: '3px',
+			},
+		},
+	},
+	// Extend other parts of the theme if needed
+});
 
 export default theme;
