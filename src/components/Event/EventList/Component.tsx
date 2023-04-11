a817b41c425880d2a845b75be97c062178cc08d2
Daniel vdg <danielvandeghinste@gmail.com>
2023-04-11 17:09:26 +0200

    refactored a bunch of things and deployment should work now

---
+++ src/components/Event/EventList/Component.tsx
@@ -0,0 +1,61 @@
+import { Box, Text, VStack, Spinner } from '@chakra-ui/react';
+import { FC } from 'react';
+import { Event } from '~/schemas/event';
+
+interface EventListProps {
+	events: Event[] | null | undefined;
+	isLoading: boolean;
+	isError: boolean;
+}
+
+export const EventList: FC<EventListProps> = ({
+	events,
+	isLoading,
+	isError,
+}) => {
+	if (isLoading) return <Spinner />;
+	if (isError) return <div>Something went wrong!</div>;
+	if (!isLoading && !isError && events && events.length === 0)
+		return <div>No events found</div>;
+
+	return (
+		<VStack spacing={6} alignItems='center' flexGrow={1}>
+			{events &&
+				events.map((event) => (
+					<Box
+						key={event.id}
+						bgColor={'blue.500'}
+						w={'61.8%'}
+						borderRadius={'md'}
+						borderWidth={'1px'}
+						borderColor={'gray.300'}
+						p={4}
+						position={'relative'}
+						boxShadow={'md'}
+					>
+						<Text
+							placeSelf={'flex-end'}
+							fontWeight={'medium'}
+							fontStyle={'italic'}
+							position={'absolute'}
+							top={2}
+							right={2}
+						>
+							{event.datetime.toDateString()}
+						</Text>
+						<Text as={'u'} fontSize={'2xl'} fontWeight={'bold'}>
+							{event.name}
+						</Text>
+						<Text mt={2}>{event.description}</Text>
+						<Text mt={2}>Location: {event.location}</Text>
+						<Text mt={2}>Reservation link: {event.slug}</Text>
+						<Text mt={2} fontWeight={'medium'}>
+							Attendees: {event.attendees}
+						</Text>
+					</Box>
+				))}
+		</VStack>
+	);
+};
+
+export default EventList;
